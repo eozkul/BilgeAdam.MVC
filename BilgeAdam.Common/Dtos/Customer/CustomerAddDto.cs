04f2f13e9fd01a57691ed84d10d533b583fbf7da
ESENGÜL ÖZKUL <esengulozkul1990@gmail.com>
2022-05-19 18:48:17 +0300

    done validations

--- BilgeAdam.Common/Dtos/Customer/CustomerAddDto.cs
+++ BilgeAdam.Common/Dtos/Customer/CustomerAddDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace BilgeAdam.Common.Dtos.Customer
 {
     public class CustomerAddDto
     {
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz!")]
         public string CustomerId { get; set; }
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz!")]
         public string CustomerName { get; set; }
         public string? ContactName { get; set; }
         public string? Address { get; set; }
